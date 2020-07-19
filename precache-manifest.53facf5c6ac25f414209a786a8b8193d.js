a7b56e145605f7804cde8ad09eec2317ad28e253
rcammack <rcammack@hawaii.edu>
2020-07-18 22:53:04 -1000

    Updates

---
+++ precache-manifest.53facf5c6ac25f414209a786a8b8193d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e129f95cde3754e66306d67d54ba0d61",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "103bb8f44b6bd18333ec",
+    "url": "/game/static/css/main.911d8eac.chunk.css"
+  },
+  {
+    "revision": "98bb33ed4e9ec2bb600e",
+    "url": "/game/static/js/2.96d05c66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game/static/js/2.96d05c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103bb8f44b6bd18333ec",
+    "url": "/game/static/js/main.0f977714.chunk.js"
+  },
+  {
+    "revision": "f3a7a62d377cd87db1aa",
+    "url": "/game/static/js/runtime-main.0051d05c.js"
+  }
+]);
